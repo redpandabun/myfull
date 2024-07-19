082385ebe88d2a1c742497f7d41824be6b4ac8c4
redpandabun <144318643+redpandabun@users.noreply.github.com>
2024-07-19 15:58:40 +0900

    chore: initial gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  @Suppress("UnstableApiUsage")
+  repositories {
+    mavenCentral()
+  }
+}
