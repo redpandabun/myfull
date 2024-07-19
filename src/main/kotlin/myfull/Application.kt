7816ac3c37dbfd38d088285e4f4c9b24edb66af3
redpandabun <144318643+redpandabun@users.noreply.github.com>
2024-07-19 18:33:59 +0900

    feat: add application ready log

--- src/main/kotlin/myfull/Application.kt
+++ src/main/kotlin/myfull/Application.kt
@@ -1,10 +1,24 @@
 package myfull
 
+import myfull.global.logging.createLogger
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
+import kotlin.time.DurationUnit.MILLISECONDS
+import kotlin.time.toKotlinDuration
 
 @SpringBootApplication
-class Application
+class Application {
+  private final val log = createLogger {}
+
+  @EventListener
+  fun onReady(e: ApplicationReadyEvent) {
+    val startedAt = e.timeTaken.toKotlinDuration().toString(MILLISECONDS)
+
+    log.info { "Application started at $startedAt" }
+  }
+}
 
 fun main(vararg args: String) {
   runApplication<Application>(*args)
