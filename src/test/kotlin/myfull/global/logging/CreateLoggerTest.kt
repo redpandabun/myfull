04d8849091c2ad16eeb9c63bb7c6422e33cbc89b
redpandabun <144318643+redpandabun@users.noreply.github.com>
2024-07-19 18:38:07 +0900

    style: indent 제거

--- src/test/kotlin/myfull/global/logging/CreateLoggerTest.kt
+++ src/test/kotlin/myfull/global/logging/CreateLoggerTest.kt
@@ -4,8 +4,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class CreateLoggerTest {
-
-
   @Test
   fun `createLogger 메소드는 올바른 클래스 이름을 반환해야 한다`() {
     assertEquals(CreateLoggerTest::class.qualifiedName, createLogger {}.name)
